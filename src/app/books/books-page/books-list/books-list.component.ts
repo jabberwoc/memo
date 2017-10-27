90f5cad2d2e5371f507ede39abbc9eba578b275e
Sebastian Stöcker <s.stoecker@surfhome.de>
2017-10-27 14:36:05 +0200

    styles

--- src/app/books/books-page/books-list/books-list.component.ts
+++ src/app/books/books-page/books-list/books-list.component.ts
@@ -3,14 +3,12 @@ import { Book } from '../../../entities/book';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DataService } from '../../../data/data.service';
 
-
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
   styleUrls: ['./books-list.component.css']
 })
 export class BooksListComponent implements OnInit {
-
   @Input() books: Array<Book>;
   @Output() deleteRequest = new EventEmitter<Book>();
 
@@ -20,18 +18,18 @@ export class BooksListComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private dataService: DataService
-  ) { }
+  ) {}
 
   select(book: Book): void {
     this.selectedBook = book;
     console.log(this.selectedBook.name + ' selected.');
+    this.open(book);
   }
 
   open(book: Book): void {
     // TODO
     console.log('opening book: ' + book.id + ', name: ' + book.name);
     this.router.navigate(['notes', book.id, { name: book.name }]);
-    // this.router.navigate(['books/', { bookId: book.id }]);
   }
 
   deleteBook(book: Book) {
@@ -39,14 +37,11 @@ export class BooksListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.params.subscribe(
-      p => {
-        console.log(p);
-        // this.id = p['id'];
-        // this.showDetails = p['showDetails'];
-        // this.load();
-      }
-    );
+    this.route.params.subscribe(p => {
+      console.log(p);
+      // this.id = p['id'];
+      // this.showDetails = p['showDetails'];
+      // this.load();
+    });
   }
-
 }
