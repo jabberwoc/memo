dd0da1d9d89fde66221d80837bad23b9ef0a5cce
Sebastian Stöcker <s.stoecker@surfhome.de>
2019-06-17 21:30:05 +0200

    cleanup

--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksPageComponent } from './books-page.component';
-import { NotesPageComponent } from '../notes/notes-page.component';
 
 const routes: Routes = [
   {
